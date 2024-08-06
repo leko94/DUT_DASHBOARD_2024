e868d8d3da5926ad2d06c6b481a8bb4766eb4dc0
leko94 <ngcobo.nkululeko@yahoo.com>
2024-08-06 17:45:54 +0200

    Update Dashboard_DUT.py

--- Dashboard_DUT.py
+++ Dashboard_DUT.py
@@ -32,7 +32,7 @@ def send_email(subject, message, recipient):
 # File paths to the Excel files
 staff_file_path = 'Chart in Microsoft PowerPoint.xlsx'
 students_file_path = 'Students.xlsx'
-performance_file_path = '/content/Student Perfomances.xlsx'
+performance_file_path = 'Student Perfomances.xlsx'
 
 # Functions to create charts for staff data
 def create_staff_charts():
